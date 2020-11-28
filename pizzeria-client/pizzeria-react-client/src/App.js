7c88811294ccdb35dfc5a95a779664a0e7d9c11a
aturner <aturner@me.com>
2020-11-28 14:44:38 +0000

    install redux, axios and set up store

--- pizzeria-client/pizzeria-react-client/src/App.js
+++ pizzeria-client/pizzeria-react-client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import store from "./store";
+import HomeView from "./components/HomeView";
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div clasName="App">
+          <Route exact path="/" component={HomeView} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ pizzeria-client/pizzeria-react-client/src/components/HomeView.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class HomeView extends Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome to the home page</p>
+      </div>
+    );
+  }
+}
+export default HomeView;
